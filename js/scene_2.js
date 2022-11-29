5a37f7561091961494d2d5bd249c0de1a212372c
olivialin21 <s110819023@stu.ntue.edu.tw>
2022-11-29 08:08:10 +0800

    add sound effect and setting

--- js/scene_2.js
+++ js/scene_2.js
@@ -1,19 +1,37 @@
-// gsap.to(img_2_4_1, {
-//   scrollTrigger: video_2,
-//   x: 500 // 向右移動 500px
-// });
+const video_2 = document.getElementById("video_2");
 
-const video_2_tl = gsap.timeline({
-  // 建立時間軸
+gsap.to(img_2_4_1 ,{
   scrollTrigger: {
     trigger: video_2,
-    // 以當前選取的物件作為觸發基準
-    start: "top top", // when the top of the trigger hits the top of the viewport
-    end: "bottom",
-    toggleActions: "restart none reverse none",
-    // markers: true,
-  }
+    start: "0% top",
+    toggleActions: "play reverse none reverse",
+  },
+  display: "block",
+  opacity: 1,
+  duration: 1
 });
+gsap.to(img_2_4_1 ,{
+  scrollTrigger: {
+    trigger: video_2,
+    start: "100% top",
+    toggleActions: "play reverse none reverse",
+  },
+  display: "block",
+  opacity: 0,
+  duration: 1
+});
+
+// const video_2_tl = gsap.timeline({
+//   // 建立時間軸
+//   scrollTrigger: {
+//     trigger: video_2,
+//     // 以當前選取的物件作為觸發基準
+//     start: "top top", // when the top of the trigger hits the top of the viewport
+//     end: "bottom",
+//     toggleActions: "restart none reverse none",
+//     // markers: true,
+//   }
+// });
 
 // video_2_tl
 // .fromTo(img_2_4_1, {
